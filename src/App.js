89508e4923df041809569adba38a5c14d3aa9844
oliver <olivercastroteran@gmail.com>
2020-10-08 14:20:22 -0400

    development mode added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 const App = () => {
   return (
     <div className="app">
-      <h1>React App from scratch</h1>
+      <h1>React App from scratch!</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
+        doloribus officia harum asperiores maiores commodi, quos aliquid earum
+        ea animi blanditiis sint, expedita hic nesciunt laborum tempore. Rem
+        molestias tenetur corporis nulla, veritatis porro animi minus rerum
+        reiciendis iste optio laudantium quae sed ratione quam harum quis
+        commodi fuga ducimus.
+      </p>
     </div>
   );
 };
